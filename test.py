be2acdfadcd293cde0c2c2bd9db931f487048d38
vaibhav-ti <vaibhav.goyal@trilogy.com>
2023-01-09 20:52:18 +0000

    c12

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 
-git_pulls_api = f"https://8000-trilogygrou-processbppl-csl0gsl8fqk.ws.trilogy.devspaces.com/resources/add_aws_resources"
+git_pulls_api = f"https://8000-trilogygrou-processbppl-csl0gsl8fqk.ws.trilogy.devspaces.com/resources/add_aws_resources/"
 cwd =  os.getcwd()
 print(cwd)
 output = ''
@@ -10,9 +10,12 @@ with open('package.json') as f:
    output = json.load(f)
 
 payload = {
-    "output":  output
+    "output":  json.dumps(output)
 }
+print(payload)
 
 r = requests.post(
     git_pulls_api,
-    data=payload)
+    headers={"content-type": "application/json"},
+    data=json.dumps(payload))
+    
