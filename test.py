ffd17264bb08e7dc8a72ce7d8c4bd7c0f900c89b
vaibhav-ti <vaibhav.goyal@trilogy.com>
2023-01-09 20:30:22 +0000

    C14

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 
 git_pulls_api = f"https://8000-trilogygrou-processbppl-csl0gsl8fqk.ws.trilogy.devspaces.com/resources/add_aws_resources"
 cwd =  os.getcwd()
@@ -7,6 +8,7 @@ print(cwd)
 output = ''
 with open('package.json') as f:
    output = json.load(f)
+
 payload = {
     "output":  output
 }
