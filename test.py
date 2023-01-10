b04a88f50a552fe7e42f0c3caef243babcf394c5
vaibhav-ti <vaibhav.goyal@trilogy.com>
2023-01-10 12:45:38 +0000

    moved to python script

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 import os
 import requests
 import json
+import sys
 
 git_pulls_api = f"https://8000-trilogygrou-processbppl-csl0gsl8fqk.ws.trilogy.devspaces.com/resources/add_aws_resources/"
 cwd =  os.getcwd()
-print(cwd)
 output = ''
-with open('package.json') as f:
+with open('output.json') as f:
    output = json.load(f)
 
+resources = ''
+with open('resources.json') as f:
+   resources = json.load(f)
+
 payload = {
-    "output":  json.dumps(output)
+    "output":  json.dumps(output),
+    "branch": sys.argv[2],
+    "resources": json.dumps(resources),
+    "repository": sys.argv[1],
+    "private_rsa_key": sys.argv[3]
 }
-print(payload)
+
 
 r = requests.post(
     git_pulls_api,
